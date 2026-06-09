cc66770a68cc2cd53ad41ee23244c8d6c8966336
Matheus Alvarenga <alvarengamatheus89@gmail.com>
2026-06-09 15:34:04 -0300

    Revert "Implementa API REST do app Estagio com models, serializers, views e urls"
    This reverts commit 19b801374bc3a0fa9b31dea2029a6634983eacab.

--- src/Estagio/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Conta, Perfil, Postagem, Interacao, Mensagem
-
-admin.site.register(Conta)
-admin.site.register(Perfil)
-admin.site.register(Postagem)
-admin.site.register(Interacao)
-admin.site.register(Mensagem)
